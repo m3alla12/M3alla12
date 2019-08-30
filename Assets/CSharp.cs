05963cdb7ce0198a3d34efae8404bfcb5b49410f
m3alla12 <54588999+m3alla12@users.noreply.github.com>
2019-08-30 10:59:41 +0100

    Add files via upload

---
+++ Assets/CSharp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSharp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 12;
+        int b = 9;
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        float f = (float)a / b;
+        int g = a % b;
+        Debug.Log(c);
+        Debug.Log(d);
+        Debug.Log(e);
+        Debug.Log(f);
+        Debug.Log(g);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
